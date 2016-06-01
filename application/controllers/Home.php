566e10aee9792dbc11114586d35af8a8f03ea37d
gwleuverink <willem@mediacode.nl>
2016-06-01 19:40:45 +0200

    Tinkered with some Guzzle settings so we can now mage API requests via the reverse proxy setup in our local network

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,28 +6,46 @@ use \GuzzleHttp\Client;
 class Home extends CI_Controller {
 
 
-	public function __construct()
-	{
-		parent::construct();
-	}
-
 	public function index()
 	{
+
 		$this->load->view('home');
 		echo "<br /><br /><br /><br />";
-		
-		
+
+		/*
 		$expected = crypt('pass_1234!', '$6$rounds=8000$randomizedsalt16$');
 		$correct = crypt('pass_1234!', '$6$rounds=8000$randomizedsalt16$');
 		$wrong = crypt('pass_4321?', '$6$rounds=8000$randomizedsalt16$');
 		echo "correct SHA-512:    " . $correct;
-		
+
 		echo "<br /><br /> comparing expected with correct hash";
 		var_dump(hash_equals($expected, $correct)); //true
-		
+
 		echo "<br /><br /> comparing expected with wrong hash";
 		var_dump(hash_equals($expected, $wrong)); //false
-		
+		*/
+
+		$username = "portal_srv_usr";
+		$password = "RkxVSCNUZWNoNjBka2hHRA==";
+
+		$client = new Client([ // Maak API client object
+			'base_uri' => "https://$username:$password@188.202.124.134/connector/public/",
+			'verify'   => false,
+			'timeout'  => 10.0
+		]);
+		// Doe verzoek, zonder auth header
+		$res = $client->request('GET', 'getmember/' . 10724);
+
+		if($res->getStatusCode() == 200) {
+			echo "<pre>";
+			echo $res->getBody();
+			echo "</pre>";
+		}
+		else {
+			//$this->logout(HTTPS_ERROR);
+			echo $res->getStatusCode();
+		}
+
 	}
 
 }
